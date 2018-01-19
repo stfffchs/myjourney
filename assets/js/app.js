824ece4c39473f6c92ee3c277096870b913dd518
HFIAD17-STEPHAN\stafu <st.a.fuchs@gmail.com>
2018-01-19 15:48:17 +0100

    scrollify

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,21 @@
 $(document).foundation()
+$(function() {
+  $.scrollify({
+    section : ".scrollify-section",
+    interstitialSection : "",
+    easing: "easeOutExpo",
+    scrollSpeed: 1100,
+    offset : 0,
+    scrollbars: true,
+    standardScrollElements: "",
+    setHeights: true,
+    overflowScroll: true,
+    updateHash: true,
+    touchScroll:true,
+    before:function() {},
+    after:function() {},
+    afterResize:function() {},
+    afterRender:function() {}
+  });
+	});
+
